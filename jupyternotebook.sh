f87f7c16c1f937198c9f86289881b927c5b6cef9
xx <xx>
2019-11-19 13:43:59 +0800

    fix gui docker

--- jupyternotebook.sh
+++ jupyternotebook.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-xhost +local:docker
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-echo sudo docker run -it --device=/dev/video0　-e DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH dockergui/v1
-xhost -local:docker
 
-#docker run -it --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook
+docker run -it --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD:/home/jovyan/work"  -v "/tmp/.X11-unix:/tmp/.X11-unix" -v "/tmp/.docker.xauth:/tmp/.docker.xauth" -e "XAUTHORITY=/tmp/.docker.xauth" --device=/dev/video0 -e DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1  jupyter/datascience-notebook
 
